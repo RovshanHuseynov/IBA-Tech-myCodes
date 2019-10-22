fdd1e928beb0d92f15bb14c572d3952df764bf6d
PC <rovsen.huseyn.oglu@gmail.com>
2019-10-22 09:43:07 +0400

    -- third. structure was changed to clean one.

--- src/main/java/ClassTest.java
+++ src/main/java/ClassTest.java
@@ -10,19 +10,19 @@ public class ClassTest {
         verify(s2);
     }
 
-    public static String call(int len){
+    public static String call(int len) {
         Random random = new Random();
-        int r1,r2,r3;
+        int r1, r2, r3;
         String s = "";
 
-        for(int i=0; i<len; i++) {
+        for (int i = 0; i < len; i++) {
             r3 = random.nextInt(2);   // take Big A or small a randomly
             if (r3 == 0) {    // take Big A
                 r1 = random.nextInt(26) + 65;
-                s += (char)r1;
+                s += (char) r1;
             } else if (r3 == 1) {  // take small a
                 r2 = random.nextInt(26) + 97;
-                s += (char)r2;
+                s += (char) r2;
             }
         }
 
@@ -30,30 +30,29 @@ public class ClassTest {
         return s;
     }
 
-    public static void verify(String s){
+    public static void verify(String s) {
         boolean flag = true;
 
         // first verification
-        for(int i=0; i<s.length(); i++){
-            if(s.charAt(i) < 65 || (s.charAt(i) > 90 && s.charAt(i) < 97) || s.charAt(i) > 122){
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) < 65 || (s.charAt(i) > 90 && s.charAt(i) < 97) || s.charAt(i) > 122) {
                 flag = false;
             }
         }
 
         // second verification
-        if(flag && s.equals(null)){
+        if (flag && s.equals(null)) {
             flag = false;
         }
 
         // third verification
-        if(flag && s.length() == 0){
+        if (flag && s.length() == 0) {
             flag = false;
         }
 
-        if(flag){
+        if (flag) {
             System.out.println("verified");
-        }
-        else{
+        } else {
             System.out.println("not verified");
         }
     }
